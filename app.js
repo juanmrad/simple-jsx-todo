d6e1a30404012a8f8476d3351542bcee03d33b6b
Juan Mrad <juansmrad@gmail.com>
2021-03-23 18:23:34 -0500

    solution

--- app.js
+++ app.js
@@ -1,42 +1,65 @@
 let todos = [
     {
         id: 1,
-        todo: 'Code Epic JSX',
+        name: 'Code Epic JSX',
         completed: false
     },
     {
         id: 2,
-        todo: 'Learn Epic JSX',
+        name: 'Learn Epic JSX',
         completed: false
     },
     {
         id: 3,
-        todo: 'Learn Props',
+        name: 'Learn Props',
         completed: true
     }
 ];
 
-let TodoItem = () => {
-    return (<span>Render single todo item</span>)
+let TodoItem = (props) => {
+    return (<li>{props.todo.name} completed: {props.todo.completed ? 'Yes' : 'No'}</li>)
 }
 
-let TodoList = () => {
-    return (<span>Render all todo items from todo list</span>)
+let TodoList = (props) => {
+    return (<ul>
+        {props.todos.map((todo, index) => {
+            return <TodoItem key={index} todo={todo} />
+        })}
+    </ul>)
 }
 
-let TodoAddTodo = () => {
-    function handleClick() {
-        console.log('I was clicked');
+let TodoAddTodo = (props) => {
+    function handleClick(e) {
+        e.preventDefault();
+        props.updateTodo({
+            name: e.target.newTodo.value,
+            completed: false
+        });
+        e.target.newTodo.value = '';
     }
 
-    return (<span><form onSubmit={handleClick}><button type="submit">Add new todo</button></form></span>)
+    return (<span><form onSubmit={handleClick}><input type="text" name="newTodo"></input><button type="submit">Add new todo</button></form></span>)
 }
 
-let TodoListApp = () => {
-    return (<span>
-        <TodoAddTodo />
-    </span>)
+let TodoListApp = (props) => {
+
+    const [todos, setTodo] = React.useState(props.todos);
+
+    function updateTodoState(newTodo) {
+        let newTodos = [...todos, {
+            id: todos.length + 1,
+            ...newTodo
+        }];
+        setTodo(newTodos);
+    };
+
+    return (
+    <div>
+        <TodoList todos={todos} />
+        <TodoAddTodo updateTodo={updateTodoState}/>
+    </div>
+    );
 }
 
 
-ReactDOM.render(<TodoListApp />, document.getElementById('root'))
+ReactDOM.render(<TodoListApp todos={todos} />, document.getElementById('root'));
